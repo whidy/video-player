2f8a75b9fd5a66ef5698d5314202250c55d055ed
whidy.desktop <bs1988@gmail.com>
2017-07-19 21:43:34 +0800

    player test

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+
+var config = {
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: require.resolve('jquery'),
+        use: [{
+          loader: 'expose-loader',
+          options: '$'
+        }]
+      }
+    ]
+  },
+  plugins: [
+  ],
+  devServer: {
+    contentBase: __dirname,
+    compress: true,
+    port: 9000,
+    inline: true,
+    host: '0.0.0.0',
+    hot: true,
+    disableHostCheck: true
+  }
+}
+
+module.exports = config;
